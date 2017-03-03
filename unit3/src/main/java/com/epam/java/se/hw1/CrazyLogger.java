c372e3fcede69ad9d509f1855c09ee60128d6359
Евгений Александров < >
2017-03-04 01:05:45 +0300

    overloaded log method has been created

--- unit3/src/main/java/com/epam/java/se/hw1/CrazyLogger.java
+++ unit3/src/main/java/com/epam/java/se/hw1/CrazyLogger.java
@@ -1,6 +1,59 @@
 package com.epam.java.se.hw1;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
 public class CrazyLogger {
 
+    private final String toFormat = "dd-mm-YYYY : hh-mm";
+    private final String name;
+    private StringBuilder logBuilder;
+
+    public CrazyLogger(String name) {
+        this.name = name;
+
+        logBuilder = new StringBuilder();
+        logBuilder.append(Level.INFO.toString()).append(":").append(name).append(":").append("has been initialized at").
+                append(LocalDateTime.now().format(DateTimeFormatter.ofPattern(toFormat))).append("\n");
+    }
+
+    public void log(String msg){
+        Objects.requireNonNull(msg, "Error: message parameter can't be a null");
+        logBuilder.append(name).append(":").append(LocalDateTime.now().format(DateTimeFormatter.ofPattern(toFormat))).append(msg).append("\n");
+    }
+
+    public void log(Level level, String msg){
+        Objects.requireNonNull(level, "Error: level parameter can't be a null");
+        logBuilder.append(level.toString()).append(":").append(name).append(":").append(LocalDateTime.now().format(DateTimeFormatter.ofPattern(toFormat))).append(msg).append("\n");
+    }
+
+    public void log(String msg, Throwable e) {
+        Objects.requireNonNull(msg, "Error: parameter - message can't be a null");
+        Objects.requireNonNull(e, "Error: parameter - exception can't be a null");
+
+        logBuilder.append(Level.WARNING.toString()).append(":").append(name).append(":").append(LocalDateTime.now().
+                format(DateTimeFormatter.ofPattern(toFormat))).append(msg).append(":").append(e).append("\n");
+    }
+
+
+    /**
+     * types of logging
+     */
+    enum Level{
+
+        INFO("INFO"), WARNING("WARNING");
+
+        private final String levelName;
+
+        Level(String name) {
+            this.levelName = name;
+        }
+
+        @Override
+        public String toString(){
+            return levelName;
+        }
+    }
 }
--- unit3/src/test/java/com/epam/java/se/hw1/CrazyLoggerTest.java
+++ unit3/src/test/java/com/epam/java/se/hw1/CrazyLoggerTest.java
@@ -2,14 +2,22 @@ package com.epam.java.se.hw1;
 
 import org.junit.Test;
 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.Assert.*;
 
 
 public class CrazyLoggerTest {
 
+
+
     @Test
     public void testThatMethodFindInfoWorksRight(){
-
+        CrazyLogger logger = new CrazyLogger(CrazyLoggerTest.class.getName());
+        String msg = "";
+        logger.log(msg);
     }
 
     @Test
