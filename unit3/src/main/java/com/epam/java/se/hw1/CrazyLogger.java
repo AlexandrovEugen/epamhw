289cc5bf3b807a52927f7e1c329dfc479721b6be
Евгений Александров < >
2017-03-03 18:55:58 +0300

    created class CrazyLogeer without  body and his test

---
+++ unit3/src/main/java/com/epam/java/se/hw1/CrazyLogger.java
@@ -0,0 +1,6 @@
+package com.epam.java.se.hw1;
+
+
+public class CrazyLogger {
+
+}
---
+++ unit3/src/test/java/com/epam/java/se/hw1/CrazyLoggerTest.java
@@ -0,0 +1,19 @@
+package com.epam.java.se.hw1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CrazyLoggerTest {
+
+    @Test
+    public void testThatMethodFindInfoWorksRight(){
+
+    }
+
+    @Test
+    public void testThatMethodLogWorksRight(){
+
+    }
+}
