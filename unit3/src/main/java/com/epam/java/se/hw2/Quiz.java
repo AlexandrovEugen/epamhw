ac1697e9d5d486929e56e91e18a4e30bbf44bab1
Евгений Александров < >
2017-03-05 15:58:07 +0300

    structure of tests is done

---
+++ unit3/src/main/java/com/epam/java/se/hw2/Quiz.java
@@ -0,0 +1,7 @@
+package com.epam.java.se.hw2;
+
+
+
+public class Quiz {
+
+}
---
+++ unit3/src/test/java/com/epam/java/se/hw2/QuizTest.java
@@ -0,0 +1,18 @@
+package com.epam.java.se.hw2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class QuizTest {
+
+    @Test
+    public void testThatMethodGetLanguageWorksRight(){}
+
+    @Test
+    public void testThatConversationIsGoingOnChosenLanguage(){}
+
+    @Test
+    public void testThatQuizWillHasFailedWithIncorrectInput(){}
+}
