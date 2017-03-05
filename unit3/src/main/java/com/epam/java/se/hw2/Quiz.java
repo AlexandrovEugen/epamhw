fdbc7f44da2204c05e1707107ac50d2e1d1ec34f
Евгений Александров < >
2017-03-05 17:18:23 +0300

    all tests are failed

--- unit3/src/main/java/com/epam/java/se/hw2/Quiz.java
+++ unit3/src/main/java/com/epam/java/se/hw2/Quiz.java
@@ -1,7 +1,24 @@
 package com.epam.java.se.hw2;
 
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Quiz {
 
+
+    private Locale locale;
+
+    public void setLocale(String ru) {
+
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public ResourceBundle getQuestions() {
+        return null;
+    }
 }
+
--- unit3/src/test/java/com/epam/java/se/hw2/QuizTest.java
+++ unit3/src/test/java/com/epam/java/se/hw2/QuizTest.java
@@ -2,17 +2,36 @@ package com.epam.java.se.hw2;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class QuizTest {
 
-    @Test
-    public void testThatMethodGetLanguageWorksRight(){}
 
     @Test
-    public void testThatConversationIsGoingOnChosenLanguage(){}
+    public void testThatMethodGetLanguageWorksRight(){
+        Quiz quiz = new Quiz();
+        quiz.setLocale("ru");
+        Locale locale = new Locale("ru");
+        assertEquals(locale, quiz.getLocale());
+    }
 
     @Test
-    public void testThatQuizWillHasFailedWithIncorrectInput(){}
+    public void testThatConversationIsGoingOnChosenLanguage(){
+        Quiz quiz = new Quiz();
+        quiz.setLocale("ru");
+        ResourceBundle ruQuestions = ResourceBundle.getBundle("ruQuestions" , quiz.getLocale());
+        assertEquals(ruQuestions, quiz.getQuestions());
+    }
+
+    @Test(expected = MissingResourceException.class)
+    public void testThatQuizWillHasFailedWithIncorrectInput(){
+        Quiz quiz = new Quiz();
+        quiz.setLocale("fr");
+        quiz.getQuestions();
+    }
 }
