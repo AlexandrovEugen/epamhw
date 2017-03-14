240170029345254b1371ba4a38e5ef9f7022c61b
Евгений Александров < >
2017-03-15 01:48:37 +0300

    hw2: all tests are green

--- unit5/src/main/java/com/epam/java/se/hw2/classes/PropertyLoader.java
+++ unit5/src/main/java/com/epam/java/se/hw2/classes/PropertyLoader.java
@@ -1,19 +1,49 @@
 package com.epam.java.se.hw2.classes;
 
 
+import com.epam.java.se.hw2.exeptions.NoSuchKeyFromPropertyFile;
+import com.epam.java.se.hw2.exeptions.PropertiesFileNotFoundException;
 
-import java.util.ResourceBundle;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
-public class PropertyLoader extends ResourceBundle {
+public class PropertyLoader{
+
+    private final Properties properties = new Properties();
+    private final Map<String, Object> store = new HashMap<>();
 
 
-    private ResourceBundle defaultBundle;
-
-    public void load(String propertyName) {
+    public void load(String s) throws PropertiesFileNotFoundException {
+        try {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(s)))){
+                properties.load(br);
+            }
+        } catch (IOException e) {
+            throw new PropertiesFileNotFoundException();
+        }
+        for (String keys : properties.stringPropertyNames()) {
+            Object value = properties.getProperty(keys);
+            store.put(keys, value);
+        }
 
     }
 
-    public ResourceBundle getDefaultBundle() {
-        return defaultBundle;
+
+    public PropertyLoader getDefaultBundle() {
+        return this;
+    }
+
+    public Object getString(String key) {
+        if(store.containsKey(key)){
+           return store.get(key);
+        }
+        else {
+            throw new NoSuchKeyFromPropertyFile(NoSuchKeyFromPropertyFile.class.getName());
+        }
     }
 }
--- unit5/src/test/java/com/epam/java/se/hw2/classes/PropertyLoaderTest.java
+++ unit5/src/test/java/com/epam/java/se/hw2/classes/PropertyLoaderTest.java
@@ -6,8 +6,6 @@ import com.epam.java.se.hw2.exeptions.PropertiesFileNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ResourceBundle;
-
 import static org.junit.Assert.assertEquals;
 
 public class PropertyLoaderTest {
@@ -17,23 +15,28 @@ public class PropertyLoaderTest {
     @Before
     public void init(){
         pl = new PropertyLoader();
-        pl.load("default.properties");
+        try {
+            pl.load("C:\\Users\\Евгений\\IdeaProjects\\epamhw\\unit5\\src\\main\\resources\\default.properties");
+        } catch (PropertiesFileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void testThatWeCanGetByExistingKey(){
-        final ResourceBundle defaultBundle = pl.getDefaultBundle();
+    public void testThatWeCanGetValueByExistingKey(){
+        final PropertyLoader defaultBundle = pl.getDefaultBundle();
         assertEquals("defaultValue1",defaultBundle.getString("key1"));
     }
 
+
     @Test(expected = PropertiesFileNotFoundException.class)
-    public void testThatWeWillHaveFailedWhenWeTryLoadNonExistingPropertyFile(){
+    public void testThatWeWillHaveFailedWhenWeTryLoadNonExistingPropertyFile() throws PropertiesFileNotFoundException {
         pl.load("nonExisting.properties");
     }
 
     @Test(expected = NoSuchKeyFromPropertyFile.class)
     public void testThatWeWillHaveFailedWhenWeTryGetNonExistingKeyFromProperties(){
-        final ResourceBundle defaultBundle = pl.getDefaultBundle();
+        final PropertyLoader defaultBundle = pl.getDefaultBundle();
         defaultBundle.getString("key15");
     }
 }
