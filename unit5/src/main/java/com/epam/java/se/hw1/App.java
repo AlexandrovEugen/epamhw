a25255aa46d093d6501211b2e1f3434d5685936b
Евгений Александров < >
2017-03-14 10:57:49 +0300

    part of functionality is working

--- unit5/src/main/java/com/epam/java/se/hw1/App.java
+++ unit5/src/main/java/com/epam/java/se/hw1/App.java
@@ -1,36 +1,115 @@
 package com.epam.java.se.hw1;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class App {
 
-    public boolean queryHandler(String command){
+    public static void main(String[] args) {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean isRunning = true;
+        System.out.println("Shell command line is greeting you!!!");
+        System.out.println(System.getProperty("user.dir"));
+        while (isRunning){
+            try {
+                isRunning = handler(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static boolean handler(String clientQuery){
+
+        boolean isRunning = true;
+
+        List<String> parse = parse(clientQuery);
+        String command = parse.get(0);
         switch (command){
-
             case "cd":{
+                String directory = parse.get(1);
             }
+
+            break;
+
             case "mkdir":{
-
+                String directory = parse.get(1);
             }
+
+            break;
+
             case "touch":{
-
+                String fileName = parse.get(1);
             }
+
+            break;
+
             case "rm":{
-
+                String name = parse.get(1);
             }
+
+            break;
+
+            case "rmdir":{
+                String name = parse.get(1);
+            }
+
+            break;
+
             case "ls":{
-
+                showContent();
             }
+
+            break;
+
             case "echo":{
 
             }
+
+            break;
+
             case "help":{
-
+                System.out.println(shellCommands());
             }
+
+            break;
+
             case "exit":{
-
+                isRunning  = false;
             }
+
+            break;
+
+            default:
+                System.out.println("Unexpected command");
         }
-        return true;
+        return isRunning;
+    }
+
+    private static void showContent() {
+    }
+
+    private  static String shellCommands() {
+        StringBuilder commands  = new StringBuilder();
+        commands.append("cd: -- Change directory: cd directory_name").append("\n").
+                 append("mkdir -- Create new folder in current directory: mkdir directory_name").append("\n").
+                 append("touch -- Create file in current directory: touch file_name").append("\n").
+                 append("rm -- remove file form current directory: rm file_name").append("\n").
+                 append("rmdir -- remove folder from current directory: rmdir directory_name").append("\n").
+                 append("ls -- List information about current directory: ls").append("\n").
+                 append("echo -- Write to the file file_name message").append("\n").
+                 append("exit");
+        return commands.toString();
+    }
+
+    private  static List<String> parse(String clientQuery) {
+        Objects.requireNonNull(clientQuery);
+        return Arrays.asList(clientQuery.split(" "));
     }
 }
--- unit5/src/test/java/com/epam/java/se/hw1/AppTest.java
+++ unit5/src/test/java/com/epam/java/se/hw1/AppTest.java
@@ -1,79 +1,68 @@
 package com.epam.java.se.hw1;
 
 
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import java.io.FileNotFoundException;
-import java.nio.file.FileAlreadyExistsException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public class AppTest {
 
     private  App app;
 
 
-    @Before
-    public void init(){
-        app = new App();
-    }
-
-
-    @Ignore
-    @Test
-    public void testThatMethodGoToWorksRight(){
-        assertTrue(app.contains("unit1"));
-
-            app.cd("unit5");
-        assertTrue(app.contains("src"));
-            app.cd("unit5\\main\\java");
-        assertTrue(app.contains("com"));
-            app.cd("..");
-        assertTrue(app.contains("unit1"));
-    }
-
-    @Ignore
-    @Test(expected = FileNotFoundException.class)
-    public void testThatAppCantDeleteNonExistingFile(){
-        app.rm("customDir");
-    }
-
-    @Ignore
-    @Test(expected = FileAlreadyExistsException.class)
-    public void testThatAppCantCreateAnExitingFile(){
-        app.mkdir("customDir");
-        app.mkdir("customDir");
-    }
-
-    @Ignore
-    @Test
-    public void testThatMethodContainsWorksRight(){
-        app.mkdir("customDir");
-        app.touch("customFile");
-        assertTrue(app.contains("customDir"));
-        assertTrue(app.contains("customFile"));
-    }
-
-    @Ignore
-    @Test
-    public void testThatMethodDeleteWorksRight(){
-        app.mkdir("customDir");
-        assertTrue(app.contains("customDir"));
-        app.rm("customDir");
-        assertFalse(app.contains("customDir"));
-    }
-
-    @Ignore
-    @Test
-    public void testThatMethodWriteToFileWorksRight(){
-        app.writeToFile("unit5\\src\\main\\resources\\customFile.txt", "Hello, World!");
-        String textFromFile = app.readFromFile("unit5\\src\\main\\resources\\customFile.txt");
-        String benchmark = "Hello, Word!";
-        assertEquals(benchmark, textFromFile);
-    }
+//    @Before
+//    public void init(){
+//        app = new App();
+//    }
+//
+//
+//    @Ignore
+//    @Test
+//    public void testThatMethodGoToWorksRight(){
+//        assertTrue(app.contains("unit1"));
+//
+//            app.cd("unit5");
+//        assertTrue(app.contains("src"));
+//            app.cd("unit5\\main\\java");
+//        assertTrue(app.contains("com"));
+//            app.cd("..");
+//        assertTrue(app.contains("unit1"));
+//    }
+//
+//    @Ignore
+//    @Test(expected = FileNotFoundException.class)
+//    public void testThatAppCantDeleteNonExistingFile(){
+//        app.rm("customDir");
+//    }
+//
+//    @Ignore
+//    @Test(expected = FileAlreadyExistsException.class)
+//    public void testThatAppCantCreateAnExitingFile(){
+//        app.mkdir("customDir");
+//        app.mkdir("customDir");
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void testThatMethodContainsWorksRight(){
+//        app.mkdir("customDir");
+//        app.touch("customFile");
+//        assertTrue(app.contains("customDir"));
+//        assertTrue(app.contains("customFile"));
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void testThatMethodDeleteWorksRight(){
+//        app.mkdir("customDir");
+//        assertTrue(app.contains("customDir"));
+//        app.rm("customDir");
+//        assertFalse(app.contains("customDir"));
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void testThatMethodWriteToFileWorksRight(){
+//        app.writeToFile("unit5\\src\\main\\resources\\customFile.txt", "Hello, World!");
+//        String textFromFile = app.readFromFile("unit5\\src\\main\\resources\\customFile.txt");
+//        String benchmark = "Hello, Word!";
+//        assertEquals(benchmark, textFromFile);
+//    }
 
 }
